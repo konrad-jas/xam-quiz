fe57ee96ad373e0df256d28e994604e7fefd37e9
Konrad Jasiński <konrjas@gmail.com>
2017-06-17 14:26:43 +0200

    Factory for IScoreAssessor

---
+++ QuizApp/QuizApp/QuizApp/Services/IScoreAssessorFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace QuizApp.Core.Services
+{
+	public interface IScoreAssessorFactory
+	{
+		IScoreAssessor GetAssessor();
+	}
+}
---
+++ QuizApp/QuizApp/QuizApp/Services/Impl/ScoreAssessorFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace QuizApp.Core.Services.Impl
+{
+	public class ScoreAssessorFactory : IScoreAssessorFactory
+	{
+		private readonly IScoreAssessorConfig _config;
+		public ScoreAssessorFactory(IScoreAssessorConfig config)
+		{
+			_config = config;
+		}
+
+		public IScoreAssessor GetAssessor()
+		{
+			return new ScoreAssessor(_config);
+		}
+	}
+}
