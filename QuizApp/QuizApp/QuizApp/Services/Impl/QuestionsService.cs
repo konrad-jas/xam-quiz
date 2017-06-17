f2f475b1932edd854cc868a8ac69b789869c6b60
Wojciech Kudirka <qdirqa@gmail.com>
2017-06-17 18:09:59 +0200

    Faster question loading

--- QuizApp/QuizApp/QuizApp/Services/Impl/QuestionsService.cs
+++ QuizApp/QuizApp/QuizApp/Services/Impl/QuestionsService.cs
@@ -14,28 +14,31 @@ namespace QuizApp.Core.Services.Impl
 		private readonly ITriviaServiceProxy _triviaServiceProxy;
 		private readonly ITokenService _tokenService;
 
+		private readonly IDictionary<QuestionDifficulty, int?> _categoryIds;
+		private readonly IDictionary<QuestionDifficulty, IList<QuestionPO>> _questions;
+
 		public QuestionsService(ITriviaServiceProxy triviaServiceProxy, ITokenService tokenService)
 		{
 			_triviaServiceProxy = triviaServiceProxy;
 			_tokenService = tokenService;
+
+			_categoryIds = new Dictionary<QuestionDifficulty, int?>();
+			_questions = new Dictionary<QuestionDifficulty, IList<QuestionPO>>();
 		}
 
-		public async Task<QuestionPO> GetQuestion(int categoryId, QuestionDifficulty questionDifficulty)
+		public async Task<QuestionPO> GetQuestion(int categoryId, QuestionDifficulty difficulty)
 		{
-			var triviaQuestions = await FetchQuestions(1, categoryId, questionDifficulty);
-			var triviaQuestion = triviaQuestions?.FirstOrDefault();
-			if (triviaQuestion?.IncorrectAnswers == null)
-				return null;
+			var sameCategory = _categoryIds.ContainsKey(difficulty) && _categoryIds[difficulty] == categoryId;
+			var anyQuestions = _questions.ContainsKey(difficulty) && _questions[difficulty].Any();
 
-			var answers = triviaQuestion.IncorrectAnswers.Select(x => new AnswerPO { Answer = WebUtility.HtmlDecode(x) }).ToList();
-			answers.Add(new AnswerPO { Answer = WebUtility.HtmlDecode(triviaQuestion.CorrectAnswer), Correct = true });
-			answers.Shuffle();
-
-			return new QuestionPO
+			if (!sameCategory || !anyQuestions)
 			{
-				Question = WebUtility.HtmlDecode(triviaQuestion.Question),
-				Answers = answers
-			};
+				await PrefetchQuestions(categoryId, difficulty);
+			}
+
+			var question = _questions[difficulty].First();
+			_questions[difficulty].RemoveAt(0);
+			return question;
 		}
 
 		public async Task WipeMemory()
@@ -43,16 +46,20 @@ namespace QuizApp.Core.Services.Impl
 			await _tokenService.ResetToken();
 		}
 
+		private async Task PrefetchQuestions(int categoryId, QuestionDifficulty difficulty)
+		{
+			var questions = await FetchQuestions(10, categoryId, difficulty);
+			_questions[difficulty] = questions.Select(x => x.ToQuestionPO()).ToList();
+			_categoryIds[difficulty] = categoryId;
+		}
+
 		private async Task<IEnumerable<TriviaQuestionDTO>> FetchQuestions(int amount, int categoryId, QuestionDifficulty questionDifficulty)
 		{
 			var difficulty = questionDifficulty.ToString().ToLower();
 			var token = await _tokenService.GetOrCreateToken();
 
-			var triviaQuestions = await _triviaServiceProxy.GetTriviaQuestions(1, categoryId, difficulty, token);
-			if (triviaQuestions == null)
-				return null;
-
-			return triviaQuestions.Results;
+			var triviaQuestions = await _triviaServiceProxy.GetTriviaQuestions(amount, categoryId, difficulty, token);
+			return triviaQuestions?.Results;
 		}
 	}
 }
