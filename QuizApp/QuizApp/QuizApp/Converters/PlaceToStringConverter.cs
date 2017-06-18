985305db10d32b462c4f0bba9e9893d795f16ec1
Wojciech Kudirka <qdirqa@gmail.com>
2017-06-18 16:35:26 +0200

    Revamp UI

---
+++ QuizApp/QuizApp/QuizApp/Converters/PlaceToStringConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace QuizApp.Core.Converters
+{
+	public class PlaceToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var place = (int)value;
+
+			switch (place)
+			{
+				case 1:
+					return "1st";
+				case 2:
+					return "2nd";
+				case 3:
+					return "3rd";
+				case 4:
+					return "4th";
+				case 5:
+					return "5th";
+			}
+
+			return place.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- QuizApp/QuizApp/QuizApp/ViewModels/QuestionViewModel.cs
+++ QuizApp/QuizApp/QuizApp/ViewModels/QuestionViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using QuizApp.Core.Services;
 using System.Threading.Tasks;
